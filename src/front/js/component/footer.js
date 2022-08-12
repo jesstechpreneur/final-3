262a07c6ef8c8e262db3702932bd93af7cbe1b55
Jess <hi@jesstechpreneur.com>
2022-08-12 18:24:33 +0000

    API Updates

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -1,8 +1,9 @@
 import React, { Component } from "react";
 
 export const Footer = () => (
-	<footer className="footer mt-auto py-3 text-center">
-		<p>
-		</p>
-	</footer>
+  <footer className="footer mt-auto py-3 text-center fixed-bottom">
+    <p>
+      Created by Jessica J, Justin T, and Noah M for 4Geeks Academy Graduation
+    </p>
+  </footer>
 );
--- src/front/js/pages/shop.js
+++ src/front/js/pages/shop.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import "../../styles/home.css";
+import { Link } from "react-router-dom";
 
 export function Shop() {
   const [Shop, setShop] = useState([]);
@@ -10,7 +11,7 @@ export function Shop() {
 
   const getShop = async () => {
     const api = await fetch(
-      `https://api.spoonacular.com/recipes/random?apiKey=${process.env.RECIPE_APP}&number=9`
+      `https://api.spoonacular.com/recipes/random?apiKey=${process.env.RECIPE_APP}&number=8`
     );
     const data = await api.json();
     setShop(data.recipes);
@@ -23,21 +24,24 @@ export function Shop() {
         <div className="row">
           {Shop.map((recipe) => {
             return (
-              <div className="col-md-4 " key={recipe.id}>
-                <div className="card mb-4 box-shadow">
-                  <img
-                    className="card-img-top"
-                    src="https://spoonacular.com/recipeImages/{recipe.id}-480x360.jpg"
-                    alt={recipe.title}
-                  ></img>
-                  <div className="card-body">
-                    <h5 className="card-title">{recipe.title}</h5>
-                    <p className="card-text">{recipe.author}</p>
-                    <a href="/cart" className="btn btn-green">
-                      Add to Favorites
-                    </a>
+              <div className="col-md-4" key={recipe.id}>
+                <Link to={'/checkout/' +  recipe.id}>
+                  <div className="card mb-4 box-shadow">
+                    <img
+                      className="card-img-top"
+                      src={recipe.image}
+                      alt={recipe.title}
+                    ></img>
+                    <div className="card-body">
+                      <h5 className="card-title">{recipe.title}</h5>
+                      <p className="card-text">Cusine: {recipe.cuisines}</p>
+                      <p className="card-text">Ready in: {recipe.readyInMinutes} minutes</p>
+                      <a href="/cart" className="btn btn-green">
+                        Add to Favorites
+                      </a>
+                    </div>
                   </div>
-                </div>
+                </Link>
               </div>
             );
           })}
